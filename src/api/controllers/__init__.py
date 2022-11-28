d710c6ebab5fe8053e4a86587d972ba9f7e1630c
Diana Badila <dianabadila@microsoft.com>
2022-11-28 21:29:22 +0100

    refactoring swagger UI

---
+++ src/api/controllers/__init__.py
@@ -0,0 +1,15 @@
+from src.api.controllers.bid_controller import blueprint \
+    as bids_blueprint
+from flask import Blueprint
+from flask_restplus import Api
+from src.api.controllers.bid_controller import bidsNamespace
+
+api_extension = Api(
+    bids_blueprint,
+    title='BIDS API',
+    version='1.0',
+    description='BIDS API',
+    doc='/swagger'
+)
+
+api_extension.add_namespace(bidsNamespace)
